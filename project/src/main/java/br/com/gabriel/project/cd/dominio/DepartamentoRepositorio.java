6fd504ab1afb4e7f21ffe979c5a8c82b967a7338
hitoshi <61585229+hitoshidevx@users.noreply.github.com>
2022-04-16 11:38:22 -0300

    Delete project directory

--- project/src/main/java/br/com/gabriel/project/cd/dominio/DepartamentoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.com.gabriel.project.cd.dominio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartamentoRepositorio extends JpaRepository<Departamento, Long>{
-
-}

cec66571278addd1b693cf7c76fa5941895b8fa8
hitoshi <g.hitoshi.yokogawa@gmail.com>
2022-04-16 11:39:28 -0300

    Iniciando exercício Nome e Idade

--- ConceitosJava/LoopsAndArrays/src/br/com/gabriel/loops/NomeAndIdade.java
+++ ConceitosJava/LoopsAndArrays/src/br/com/gabriel/loops/NomeAndIdade.java
@@ -1,10 +1,17 @@
 package br.com.gabriel.loops;
 
+import java.util.Scanner;
+
 public class NomeAndIdade {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		int idade;
+		
 	}
 
 }
